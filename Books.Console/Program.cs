42b86e2d6661161019cf10404f5d6a5ddab4d5c4
gparlakov <gparlakov@gmail.com>
2018-07-25 15:18:12 +0300

    Show part of the final app - the search by author feature

--- Books.Console/Program.cs
+++ Books.Console/Program.cs
@@ -3,92 +3,79 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
+using System.Globalization;
 #endregion
 namespace Books.ConsoleApp
 {
     class Program
     {
         private static IBookPersist bookPersist = new BooksJsonPersist();
-
-        // we will use these as local global state
-        private static List<BooksByAuthor> BooksByAuthorCatalog;
+        private static readonly int lines = 20;
 
         public static void Main()
         {
-            Book[] booksAll = bookPersist.Read();
+            var booksAll = bookPersist.Read();
 
-            // catalogue - we would need at most booksAll lenght for the catalog -
-            // that would be the case that all books are by a different author
-            BooksByAuthorCatalog = new List<BooksByAuthor>();
-
-            // look at booksArray from the perspective of IEnumerable - it is still an array but we choose to
-            // only expose the IEnumerable interface through the booksEnumerable
-            IEnumerable<Book> booksEnumerable = booksAll;
-
-            // the 'only' thing we can do with an enumerable is - get its enumerator
-            foreach (var b in booksEnumerable)
+            string lineRead = string.Empty;
+            Console.WriteLine("Search by author. Type to see author list.");
+            do
             {
-                var authorAlreadyCataloguedIndex = BooksByAuthorCatalog
-                    .FindIndex(entry => entry.author == b.author);
-                // if not such index if found then authorAlreadyCataloguedIndex would be -1
-                var authorAlreadyCatalogued = authorAlreadyCataloguedIndex > -1;
-
-                if (authorAlreadyCatalogued)
+                if (lineRead.Count() >= 3)
                 {
-                    AddNewTitleToAuthor(b, authorAlreadyCataloguedIndex);
+                    var authors = SearchByAuthor.AutocompleteAuthors(booksAll, lineRead);
+                    if (authors.Count() == 0)
+                    {
+                        Console.WriteLine("No author with such name found. Try:");
+                        SearchByAuthor.SuggestAuthors(booksAll)
+                            .ToList()
+                            .ForEach(a => PrintInBackground(a + Environment.NewLine, ConsoleColor.DarkBlue));
+                        PrintOutEmptyLines(lines - 5);
+                    }
+                    else if (authors.Count() == 1)
+                    {
+                        Console.WriteLine($"Books found by {authors.First()}:");
+
+                        SearchByAuthor.Search(booksAll, authors.First())
+                            .ToList()
+                            .ForEach(book =>
+                            {
+                                PrintInBackground(
+                                    $"{book.title} of year {book.year} pages {book.pages} in {book.language } [{book.country}] {Environment.NewLine}Categories: {string.Join(",", book.categories)} {Environment.NewLine}",
+                                    ConsoleColor.DarkGreen);
+                                Console.WriteLine("------------");
+                            });
+                    }
+                    else
+                    {
+                        Console.WriteLine("Found authors:");
+                        authors.ToList().ForEach(Console.WriteLine);
+                        PrintOutEmptyLines(lines - authors.Count());
+                    }
                 }
                 else
                 {
-                    CatalogueNewAuthor(b);
+                    PrintOutEmptyLines(lines);
                 }
-            }
+                Console.WriteLine("Type author name or part of it. Type 'exit' to exit..");
+                lineRead = Console.ReadLine();
 
-            // now we have a list that has all the authors catalogued
-            OutputBooksByAuthor();
-
-            Console.WriteLine("Finished cataloguing authors. (press a key to exit...)");
-            Console.ReadLine();
+            } while (!lineRead.ToLower().Contains("exit"));
         }
 
-        private static void OutputBooksByAuthor()
+
+        private static void PrintOutEmptyLines(int number)
         {
-            foreach (var ba in BooksByAuthorCatalog)
-            {
-                Console.Write("Author: {0,-28} Books: ", ba.author);
-                foreach (var book in ba.books)
-                {
-                    Console.Write(book.title + ", ");
-                }
-                Console.Write(Environment.NewLine);
-            }
+            Enumerable.Range(0, number).ToList().ForEach(_ => Console.WriteLine());
         }
 
-        private static void AddNewTitleToAuthor(Book b, int authorCatalogIndex)
+        private static void PrintInBackground(string text, ConsoleColor color)
         {
-            var booksByThisAutorList = BooksByAuthorCatalog[authorCatalogIndex];
-            // take the current books by this author and add the next
-            booksByThisAutorList.books.Add(b);
+            var current = Console.BackgroundColor;
+            Console.BackgroundColor = color;
+            Console.Write(text);
+            Console.BackgroundColor = current;
         }
 
-        private static void CatalogueNewAuthor(Book b)
-        {
-            // there are NONE books by this author already found and cataloged
-            var newBookList = new List<Book> { b };
-            var authorAndBooks = new BooksByAuthor(b.author, newBookList);
-            // push that to the catalog
-            BooksByAuthorCatalog.Add(authorAndBooks);
-        }
-    }
-
-    public class BooksByAuthor
-    {
-        public readonly string author;
-        public readonly List<Book> books;
-
-        public BooksByAuthor(string author, List<Book> books)
-        {
-            this.author = author;
-            this.books = books;
-        }
     }
 }
---
+++ Books.Console/SearchByAuthor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Books.ConsoleApp
+{
+    // a helper type to avoid the long and non-descriptive type Dictionary ...
+    // this type has a name that suggests what it is - a catalogue by author
+    using AuthorCatalogue = IDictionary<string, IEnumerable<Book>>;
+
+    public static class SearchByAuthor
+    {
+        public static IEnumerable<Book> Search(IEnumerable<Book> inputBooks, string author)
+        {
+            return inputBooks
+                .CatalogueByAuthor()
+                .Search(author);
+        }
+
+        public static IEnumerable<string> AutocompleteAuthors(IEnumerable<Book> inputBooks, string searchPart)
+        {
+            return inputBooks
+                .ExtractAuthors()
+                .ListByPartOfName(searchPart);
+        }
+
+        public static IEnumerable<string> SuggestAuthors(IEnumerable<Book> books, int suggestionLength = 5)
+        {
+            return books
+                 .ExtractAuthors()
+                 .OrderBy(a => new Guid().ToString())
+                 .Take(suggestionLength);
+        }
+    }
+
+    internal static class SearchByAuthorBooksLinqExtensions
+    {
+        public static AuthorCatalogue CatalogueByAuthor(this IEnumerable<Book> books)
+        {
+           return books
+                .ToLookup(b => b.author)
+                .ToDictionary(authorGroup => authorGroup.Key, authorGroup => authorGroup as IEnumerable<Book>);
+        }
+
+        public static IEnumerable<Book> Search(this AuthorCatalogue catalogue, string author)
+        {
+            return catalogue.ContainsKey(author)
+                ? catalogue[author]
+                : Enumerable.Empty<Book>();
+        }
+
+        public static IEnumerable<string> ExtractAuthors(this IEnumerable<Book> books)
+        {
+            return books.Select(b => b.author).Distinct();
+        }
+
+        public static IEnumerable<string> ListByPartOfName(this IEnumerable<string> authors, string partOfName)
+        {
+            var partOfNameLower = partOfName.ToLower();
+            return authors.Select(a => a).Where(a => a.ToLower().Contains(partOfNameLower));
+        }
+    }
+}
