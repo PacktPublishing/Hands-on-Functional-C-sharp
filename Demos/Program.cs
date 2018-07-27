62fc73a95604708732d085dda652d9dc0944a99d
gparlakov <gparlakov@gmail.com>
2018-07-27 11:24:41 +0300

    Add the demos project for short code demoes outside of the main project

---
+++ Demos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+
+
+            string[] authors = new string[] {
+                "Kurt Vonnegut",
+                "Stephen King",
+                "J.K. Rowling",
+                "George R.R. Martin",
+                "Dan Brown",
+                "Mark Twain",
+                "Homer"
+            };            
+
+            var authorsLongNamesUppercased = from a in authors
+                    where a.Length > 10
+                    orderby a.Length descending
+                    select a.ToUpper()
+                    ;
+
+
+
+
+
+
+
+            //IEnumerable<string> authors = new string[100];
+
+            //var authorsShortNamesLowercased = authors
+            //        .Where(a => a.Length <= 10)
+            //        .OrderByDescending(a => a.Length)
+            //        .Select(a => a.ToLower());
+
+        }
+    }
+}
