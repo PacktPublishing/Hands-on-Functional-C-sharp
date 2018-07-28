916d94a21cb49c91815689a3f0c8a4d5ee3b36d7
Georgi Parlakov <gparlakov@gmail.com>
2018-07-28 09:02:13 +0300

    Remove methods - not helping

--- Demos/Program.cs
+++ Demos/Program.cs
@@ -18,34 +18,24 @@ namespace Demos
                 "Homer"
             };
 
-            var longNamedAuthors = FindLongNames(authors);
-            var shortNamedAuthors = FindShortNamedAuthors(authors);
+
+            var longNamedAuthors = from a in authors
+                                             where a.Length > 10
+                                             orderby a.Length descending
+                                             select a.ToUpper();
+
+
+
+            var shortNamedAuthors = authors
+                .Where(a => a.Length <= 10)
+                .OrderByDescending(a => a.Length)
+                .Select(a => a.ToLower());
 
             PrintOut(authors, "All Authors");
             PrintOut(longNamedAuthors, "Long Named authors UPPERCASED");
             PrintOut(shortNamedAuthors, "Short Named authors lowercased");
 
             Console.ReadLine();
-        }
-
-        private static IEnumerable<string> FindLongNames(string[] authors)
-        {
-            var authorsLongNamesUppercased = from a in authors
-                                             where a.Length > 10
-                                             orderby a.Length descending
-                                             select a.ToUpper();
-
-            return authorsLongNamesUppercased;
-        }
-
-        private static IEnumerable<string> FindShortNamedAuthors(string[] authors)
-        {
-            var authorsShortNamedLowercased = authors
-                .Where(a => a.Length <= 10)
-                .OrderByDescending(a => a.Length)
-                .Select(a => a.ToLower());
-
-            return authorsShortNamedLowercased;
         }
 
         private static void PrintOut(IEnumerable<string> strings, string title)
