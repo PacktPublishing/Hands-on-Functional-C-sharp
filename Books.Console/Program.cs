8cf2853c9cb65c7b3133bba75a8936ca875b9379
Georgi Parlakov <gparlakov@gmail.com>
2018-08-16 18:27:12 +0300

    Simplify and reorder for ease of explanation

--- Books.Console/Program.cs
+++ Books.Console/Program.cs
@@ -10,15 +10,13 @@ namespace Books.ConsoleApp
     {
         private static IBooksSource BooksSource = new BooksJsonSource();
 
-        // we will use these as global state
+        // we will use this as global state
         private static List<BooksByAuthor> BooksByAuthorCatalog;
 
         public static void Main()
         {
             Book[] books = BooksSource.Read();
-
-            // catalogue - we would need at most books.lenght for the catalog -
-            // that would be the case that all books are by a different author
+            
             BooksByAuthorCatalog = new List<BooksByAuthor>();
             
             for (int i = 0; i < books.Length; i++)
@@ -29,7 +27,8 @@ namespace Books.ConsoleApp
                 {
                     // there are some(1 or more) books by this author already found and catalogued
                     var authorCatalogIndex = LocateAuthorAlreadyCataloged(book);
-                    AddNewTitleToAuthor(book, authorCatalogIndex);
+
+                    BooksByAuthorCatalog[authorCatalogIndex].Books.Add(book);
                 }
                 else
                 {
@@ -37,56 +36,21 @@ namespace Books.ConsoleApp
                 }
             }
 
-            // now we have an array that has all the authors catalogued
-
+            // now we have an list that has all the authors catalogued
             OutputBooksByAuthor();
 
             Console.WriteLine("Finished cataloguing authors. (press a key to exit...)");
             Console.ReadLine();
         }
 
-        private static void OutputBooksByAuthor()
-        {
-            for (int i = 0; i < BooksByAuthorCatalog.Count; i++)
-            {
-                BooksByAuthor ba = BooksByAuthorCatalog[i];
-                Console.Write("Author: {0,-28} Books: ", ba.author);
-                for (int j = 0; j < ba.books.Length; j++)
-                {
-                    Console.Write(ba.books[j].title + ", ");
-                }
-                Console.Write(Environment.NewLine);
-            }
-        }
-
-        private static void AddNewTitleToAuthor(Book b, int authorCatalogIndex)
-        {
-            // take the current books by this author
-            Book[] books = BooksByAuthorCatalog[authorCatalogIndex].books;
-            // create a new array with lenght greater with 1
-            var newBooksArray = new Book[books.Length + 1];
-            // copy the books in the new array
-            for (int j = 0; j < books.Length; j++)
-            {
-                newBooksArray[j] = books[j];
-            }// what would j be here
-
-            // copy the current book in the last spot
-            newBooksArray[newBooksArray.Length - 1] = b;
-
-            var booksByThisAuthorUpdated = new BooksByAuthor(b.author, newBooksArray);
-            BooksByAuthorCatalog[authorCatalogIndex] = booksByThisAuthorUpdated;
-        }
+       
 
         private static void CatalogueNewAuthor(Book b)
         {
             // there are NONE books by this author already found and cataloged
-
-            // create a new array with lenght 1
-            var newBooksArray = new Book[1];
-            // put the book we just found in it
-            newBooksArray[0] = b;
-            var authorAndBooks = new BooksByAuthor(b.author, newBooksArray);
+           
+            var newBooksList = new List<Book> { b };
+            var authorAndBooks = new BooksByAuthor(b.author, newBooksList);
             // push that to the catalog
             BooksByAuthorCatalog.Add(authorAndBooks);
         }
@@ -99,7 +63,7 @@ namespace Books.ConsoleApp
             for (int j = 0; j < BooksByAuthorCatalog.Count; j++)
             {
                 var entry = BooksByAuthorCatalog[j];
-                if (entry != null && entry.author == b.author)
+                if (entry != null && entry.Author == b.author)
                 {
                     authorAlreadyCatalogued = true;
                     break;
@@ -117,7 +81,7 @@ namespace Books.ConsoleApp
             for (int j = 0; j < BooksByAuthorCatalog.Count; j++)
             {
                 var entry = BooksByAuthorCatalog[j];
-                if (entry != null && entry.author == b.author)
+                if (entry != null && entry.Author == b.author)
                 {
                     authorCatalogIndex = j;
                     break;
@@ -126,17 +90,32 @@ namespace Books.ConsoleApp
 
             return authorCatalogIndex;
         }
+
+
+        private static void OutputBooksByAuthor()
+        {
+            for (int i = 0; i < BooksByAuthorCatalog.Count; i++)
+            {
+                BooksByAuthor ba = BooksByAuthorCatalog[i];
+                Console.Write("Author: {0,-28} Books: ", ba.Author);
+                for (int j = 0; j < ba.Books.Count; j++)
+                {
+                    Console.Write(ba.Books[j].title + ", ");
+                }
+                Console.Write(Environment.NewLine);
+            }
+        }
     }
 
     public class BooksByAuthor
     {
-        public readonly string author;
-        public readonly Book[] books;
+        public readonly string Author;
+        public readonly List<Book> Books;
 
-        public BooksByAuthor(string author, Book[] books)
+        public BooksByAuthor(string author, List<Book> books)
         {
-            this.author = author;
-            this.books = books;
+            Author = author;
+            Books = books;
         }
     }
 }
