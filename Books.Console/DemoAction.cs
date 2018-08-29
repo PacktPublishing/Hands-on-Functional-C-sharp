9b32bcdda730649b3147c211d3733851e96622be
Georgi Parlakov <gparlakov@gmail.com>
2018-08-29 21:17:33 +0300

    Add demoes of Func and Action

---
+++ Books.Console/DemoAction.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Books.ConsoleApp;
+
+public class DemoAction
+{
+    private Action justAnAction;
+
+    private Action<T> AsAction<T>()
+    {
+        var name = typeof(T).Name;
+        return v => Console.WriteLine(name);
+    }
+
+    private void ContrivedMethod()
+    {
+        var willTakeString = AsAction<string>();
+
+        willTakeString("Hello world");
+        // outputs: String
+
+        //type safety - squiggly
+        willTakeString(1);
+    }
+
+    Action<string> takesString; // and does stuff with it
+    Action<int, string> takesIntAndString; // and does stuff with it
+
+    Action<object> takesAnyObject;
+
+    Action<int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int> takesMaximum;
+    // I've never used so many - so don't worry
+}
