40520bb54190123695cbcab0f03961a3d883481f
Georgi Parlakov <Georgi.Parlakov@bulpros.com>
2018-10-02 16:47:53 +0300

    start on methods - for non static class and a static class

---
+++ Demos/DemoRecipeForTurningARegularMethodIntoLINQExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demos
+{
+    public class MethodIsInNonStaticClass
+    {      
+        public IEnumerable<int> Map(IEnumerable<string> incoming)
+        {
+            return incoming.Select(s => s.Length);
+        }   
+
+    }
+
+
+    public static class MethodIsInNonStaticClass
+    {
+
+    }
+}
