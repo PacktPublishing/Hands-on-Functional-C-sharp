10a37a0e385c5bfdb9c8d37240ee83b2f4355e42
Georgi Parlakov <gparlakov@gmail.com>
2018-10-04 05:39:45 +0300

    Add a usage example

--- Demos/Program.cs
+++ Demos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Demos;
 
 namespace Demos
@@ -7,7 +8,24 @@ namespace Demos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var titles = new string[] { "The three body problem", "The dark forest", "Death's end" };
+
+            // need a new instance first and call its method then
+            var instance = new MethodIsInNonStaticClass();
+            var lenghts = instance.Lenghts(titles);
+
+            // need to name the full name of class and then the name of method
+            var longest = MethodIsInAStaticClass.GetLongest(titles);
+
+            // somewhat non-trivial use of LINQ
+            var stringsAndLengths = titles
+                // zip-up together the titles and their lengths
+                .Zip(lenghts, (t, len) => $"{t}[{len}]")
+                // aggregate (or reduce) to a single string
+                .Aggregate((prev, next) => string.Concat(prev, Environment.NewLine, next));
+
+            Console.WriteLine(stringsAndLengths);
+            Console.WriteLine("And the longest is " + longest);
             Console.ReadLine();
         }
     }
