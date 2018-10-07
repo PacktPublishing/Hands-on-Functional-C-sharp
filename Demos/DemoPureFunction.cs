643a4fb889db2022bbba34aa562836457d2bbf4f
Georgi Parlakov <gparlakov@gmail.com>
2018-10-07 20:59:45 +0300

    Add the demo

---
+++ Demos/DemoPureFunction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demos
+{
+    public class DemoPureFunction
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        // some real interesting properies:
+        // 1.Carry all their dependencies as parameters
+        // 2.Do not reach or mutate outside state
+
+
+        // However many times get called - the result is boringly unsurprising
+        // However shared state is mutated - the result is boringly unsurprising 
+        // Caching the result is ok - since it will still be the same 
+    }
+}
--- Demos/Program.cs
+++ Demos/Program.cs
@@ -8,24 +8,6 @@ namespace Demos
     {
         static void Main(string[] args)
         {
-            var titles = new string[] { "The Three-Body Problem", "The Dark forest", "Death's End" };
-
-            // need a new instance first and call its method then
-            var instance = new MethodIsInNonStaticClass();
-            var lenghts = instance.Lenghts(titles);
-
-            // need to name the full name of class and then the name of method
-            var longest = MethodIsInAStaticClass.LongestBy(titles, t => t.Length);
-
-            // somewhat non-trivial use of LINQ
-            var stringsAndLengths = titles
-                // zip-up together the titles and their lengths
-                .Zip(titles.Lenghts(), (t, len) => $"{t}[{len}]")
-                // aggregate (or reduce) to a single string
-                .Aggregate((prev, next) => string.Concat(prev, Environment.NewLine, next));
-
-            Console.WriteLine(stringsAndLengths);
-            Console.WriteLine("And the longest is " + titles.LongestBy(t => t.Length));
             Console.ReadLine();
         }
     }
