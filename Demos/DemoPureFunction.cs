2dff00f7bf1eaecb621afab5df98e3b201d741e2
Georgi Parlakov <gparlakov@gmail.com>
2018-10-07 21:23:16 +0300

    1. Some more comments for demo 2. Make the Output's Catalog global state local variable/parameter

--- Demos/DemoPureFunction.cs
+++ Demos/DemoPureFunction.cs
@@ -20,5 +20,6 @@ namespace Demos
         // However many times get called - the result is boringly unsurprising
         // However shared state is mutated - the result is boringly unsurprising 
         // Caching the result is ok - since it will still be the same 
+        // One function at a time - makes our code easy to think about
     }
 }
