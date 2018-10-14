bd3efc69fb2a2faa3319b630f019773128cdc4bf
Georgi Parlakov <gparlakov@gmail.com>
2018-10-14 22:21:57 +0300

    Initial pure method

---
+++ Demos/DemoPure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demos
+{
+    public class Pure
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
