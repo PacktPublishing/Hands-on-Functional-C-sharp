bbf0831fe2fcade173a65d3af47746f8595de899
Georgi Parlakov <gparlakov@gmail.com>
2018-10-14 22:31:25 +0300

    add a use case

--- Demos/Program.cs
+++ Demos/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Linq;
+using Books.ConsoleApp;
 using Demos;
 
 namespace Demos
 {
     class Program
     {
+        private readonly Pure pure = new Pure();
+        private readonly PureIsh pureIsh = new PureIsh(
+            () => new[] { new Book { title = "this is the one" } }
+        );
+
         static void Main(string[] args)
         {
             Console.WriteLine("demo");
