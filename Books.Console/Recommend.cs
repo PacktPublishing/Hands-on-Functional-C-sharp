320c1adf56930181d1e62647f035e648e71f2694
Georgi Parlakov <gparlakov@gmail.com>
2018-10-21 11:14:17 +0300

    Make an extension method for readability purposes for the more challenging ordering expression

--- Books.Console/Recommend.cs
+++ Books.Console/Recommend.cs
@@ -3,18 +3,23 @@ using System.Linq;
 
 namespace Books.ConsoleApp
 {
-    public class Recommend
+    public static class Recommend
     {
         public static IEnumerable<Book> ByCategoryAndYear(
             IEnumerable<Book> books, IEnumerable<string> categories, int count = 10)
         {
             return books
-                // similar in category
-                // order by most instersecting categories
-                .OrderByDescending(b => b.categories.Intersect(categories).Count())
+                .SortByMostMatchigCategories(categories)
                 // if same categories - order by year - newest first
                 .ThenByDescending(b => b.year)
                 .Take(count);
+        }
+
+        private static IOrderedEnumerable<Book> SortByMostMatchigCategories(this IEnumerable<Book> books,
+            IEnumerable<string> categories)
+        {
+            // order by most instersecting categories
+            return books.OrderByDescending(b => b.categories.Intersect(categories).Count());
         }
     }
 }
