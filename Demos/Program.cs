d73870a9586bf651c6c0bd0efbe2493397b08842
Georgi Parlakov <gparlakov@gmail.com>
2018-10-21 13:48:21 +0300

    Move the books to Demos - Paths are different when running debug from VS Code and VS

--- Demos/Program.cs
+++ Demos/Program.cs
@@ -9,22 +9,23 @@ namespace Demos
     {
         static void Main(string[] args)
         {
-            var books = new BooksJsonSource(Path.Combine(Directory.GetCurrentDirectory(), "..", "Books.Console", "books.json"))
-                .Read();
+            var books = new BooksJsonSource().Read();
 
             var huckFinn = books.First(b => b.title.Contains("Finn"));
             var rest = books.Where(b => b.title != huckFinn.title);
 
             huckFinn.categories = huckFinn.categories.Take(3).ToArray();
 
-            System.Console.WriteLine(BookMap.CategoryAuthorAndTitle(huckFinn));
-            System.Console.WriteLine("------------");
+            Console.WriteLine(BookMap.CategoryAuthorAndTitle(huckFinn));
+            Console.WriteLine("------------");
 
             Recommend
                 .ByCategoryAndYear(rest, huckFinn.categories, 3)
                 .ToAuthorTitleCategoriesYearString()
                 .ToList()
-                .ForEach(System.Console.WriteLine);
+                .ForEach(Console.WriteLine);
+
+            Console.ReadLine();
         }
     }
 }
