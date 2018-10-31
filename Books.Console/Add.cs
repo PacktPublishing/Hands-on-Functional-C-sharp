cfaac4871bf6b3bffa8a83524ae9835fb204ecfe
Georgi Parlakov <gparlakov@gmail.com>
2018-10-31 05:51:02 +0200

    Make the add tightly coupled with console and BookJsonSource for demo purposes

--- Books.Console/Add.cs
+++ Books.Console/Add.cs
@@ -4,24 +4,37 @@ namespace Books.ConsoleApp
 {
     public class Add
     {
-        public static void Book(Func<string, string> prompt, Action<Book> add)
+        public static void Book()
         {
             var newBook = new Book();
-            newBook.title = prompt("Book title: ");
-            newBook.author = prompt("Author: ");
-            newBook.categories = prompt("Categories:")
+
+            newBook.title = Prompt("Book title: ");
+            newBook.author = Prompt("Author: ");
+            newBook.categories = Prompt("Categories:")
                 .Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
 
-            var yearMaybe = prompt("Year: ");
+            var yearMaybe = Prompt("Year: ");
             int year;
             // basic validation
-            while(!int.TryParse(yearMaybe, out year))
+            while (!int.TryParse(yearMaybe, out year))
             {
-                yearMaybe = prompt("Invalid year. Please try again to specify year:");
+                yearMaybe = Prompt("Invalid year. Please try again to specify year:");
             }
             newBook.year = year;
 
-            add(newBook);
+            Persist(newBook);
+        }
+
+        private static string Prompt(string info)
+        {
+            Console.WriteLine(info);
+            return Console.ReadLine();
+        }
+
+        private static void Persist(Book book)
+        {
+            var source = new BooksJsonSource();
+            source.Add(book);
         }
     }
 }
