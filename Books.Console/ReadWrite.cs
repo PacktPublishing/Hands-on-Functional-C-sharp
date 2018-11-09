ec9b44194f99cf50fdfa1fc62c059acc9b0d1dbb
Georgi Parlakov <gparlakov@gmail.com>
2018-11-09 05:21:37 +0200

    Add an DI exaple

---
+++ Books.Console/ReadWrite.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Books.ConsoleApp
+{
+    public class ReadWrite : IReadWrite
+    {
+        public string Read()
+        {
+            return Console.ReadLine();
+        }
+
+        public void Write(string output)
+        {
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Books.Console/Setup.cs
@@ -0,0 +1,48 @@
+﻿using Books.Core;
+using Books.Infrastructure;
+using System;
+using System.Collections.Generic;
+
+namespace Books.ConsoleApp
+{
+    public interface IReadWrite
+    {
+        string Read();
+        void Write(string output);
+    }
+
+    public class SelectWithDI
+    {
+        private readonly IBooksSource bookSource;
+        private readonly Action<string> write;
+        private readonly Func<string> read;
+
+        public SelectWithDI(IBooksSource bookSource, IReadWrite readWrite)
+        {
+            this.bookSource = bookSource;
+            this.write = readWrite.Write;
+            this.read = readWrite.Read;
+        }
+
+        public Book DoSelect()
+        {
+            var books = bookSource.Read();
+
+            Func<string, IEnumerable<Book>> searchByTitle = 
+                s => Search.ByTitle(books, s);
+
+            return Select.ByTitle(write, read, searchByTitle);
+        }
+    }
+
+
+    public class DependencyInjection
+    {
+        public static void Setup(dynamic container)
+        {
+            container.Register<IBooksSource>(new BooksJsonSource());
+            
+            container.Register<IReadWrite>(new ReadWrite());
+        }
+    }
+}
